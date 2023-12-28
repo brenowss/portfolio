57896e136872517c205e2020f2051cef98e5e04e
brenowss <brenoc.fiorese@hotmail.com.br>
2023-12-27 21:41:20 -0300

    refactor: :mag: changed Metadata info to improve SEO

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import { i18n } from "../../i18n-config";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
@@ -34,30 +33,3 @@ export default function Root({
     </html>
   );
 }
-
-export const metadata: Metadata = {
-  title: "Breno Fiorese",
-  description: "Breno Fiorese - Software Engineer",
-  openGraph: {
-    title: "Breno Fiorese",
-    description: "Breno Fiorese - Software Engineer",
-    type: "website",
-    locale: "en_US",
-    url: "https://brenofiorese.dev",
-    images: [
-      {
-        url: "/og.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Breno Fiorese",
-      },
-    ],
-  },
-  icons: {
-    apple: {
-      url: "/apple-touch-icon.png",
-      sizes: "57x57",
-      type: "image/png",
-    },
-  },
-};
